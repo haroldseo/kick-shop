ed4bf569393dbf8483a66aed3bca0f2ca58b1072
Harold Seo <harold.seo.612@gmail.com>
2022-05-25 17:34:44 -0700

    Install axios and fetch data from FakeStore API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import Home from "./Home/Home";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("https://fakestoreapi.com/products");
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <Router>
